a1c0c6c06d910701678a3b3a37bdbb072aab1bf3
Rangamaniramanujam <30699043+Rangamaniramanujam@users.noreply.github.com>
2017-09-02 19:34:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,21 +17,7 @@ New File name :  main.js //save this
                        
  //you get //Hellow World Reload
 
-var myvariable = 'bob';
-
-            Varaible                  Explanation                 Example
-              
-             string                    textvr                my variable = 'Bob'/ note' quote '
-              Nunber  var                                     myvariable = 10
-
-             Boolean              True /False            var = myvariable = true;
-
-                 
-               Array                 
-                             A structure to store mulptiple values           myvariablr[0],myvarible [1] ,etc
-              ObCject   any thinmg in JS is an object and can be stored in a varible    var.myvariable =document .qurryselector ('h1);
-
-
+.
   Comments 
 1    /*  every thing in betwee is a commenmt . */
 2 For Comment with mout break //
