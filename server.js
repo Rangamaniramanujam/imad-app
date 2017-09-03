6debfc0b6d175bd659d4f6e35d11bd053099b98f
Rangamaniramanujam <30699043+Rangamaniramanujam@users.noreply.github.com>
2017-09-03 21:44:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,36 +1,18 @@
-  Go to Test Site ;
-Create folder Name ; Scripts
-New File name :  main.js //save this 
-
-                index.html
-
-
-
- 1 var myheading = document>querySelector ('h1');
-  2 myHeading .textcontent = 'hellow worid Reload';
-
-<script src ="scripts/main.js></script>
-<body> tag:
- 
- NOW Load Index File in the browser  
-                       
- //you get //Hellow World Reload
-
-var myvariable = 'bob';
-
-            Varaible                  Explanation                 Example
-              
-             string                    textvr                my variable = 'Bob'/ note' quote '
-              Nunber  var                                     myvariable = 10
-
-             Boolean              True /False            var = myvariable = true;
-
-                 
-               Array                 
-                             A structure to store mulptiple values           myvariablr[0],myvarible [1] ,etc
-              ObCject   any thinmg in JS is an object and can be stored in a varible    var.myvariable =document .qurryselector ('h1);
+<!Doctype html>
+<html>
+<head>
+    <title>Welcome
+    </title>
+    </head>
+    
+<body>
+<h1>My First Heading Mani.
+</h1>
 
 
-  Comments 
-1    /*  every thing in betwee is a commenmt . */
-2 For Comment with mout break //
+<p>My fist paragraph <p1>
+     </body>
+</html>
+
+
+        
