65f2fef1e4cb8ace0a316e0bc00f8a2616a19cd4
Rangamaniramanujam <30699043+Rangamaniramanujam@users.noreply.github.com>
2017-09-05 13:48:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,16 @@
-console.log('Loaded!');
- Go to Test Site ;
-Create folder Name ; Scripts
-New File name :  main.js //save this 
+console.l0g('Loaded!');
+//change the text of the main -text.div
 
-                index.html
+var element =document.getelementByid (main-text);
 
-
-
- 1 var myheading = document>querySelector ('h1');
-  2 myHeading .textcontent = 'hellow worid Reload';
-
-<script src ="scripts/main.js></script>
-<body> tag:
- 
- NOW Load Index File in the browser  
-                       
- //you get //Hellow World Reload
-
-.
-  Comments 
-1    /*  every thing in betwee is a commenmt . */
-2 For Comment with mout break //
+element.innerhtml ='new value';
+//move the image
+var img =document.getelementbyid('madi');
+var marginleft=0;
+function moveright () {
+marginleft = marginleft+10;
+img.stylemarginleft = marginleft+"px";
+ }
+img.onclick = function (){
+    var interval = setinterval  (moveright,100);
+}
